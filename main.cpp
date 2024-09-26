9d76c1bd2241392c7c037a7e2ea3f45b424e9c86
extend <82388064+extendlol@users.noreply.github.com>
2024-09-26 12:20:06 +0000

    a

--- main.cpp
+++ main.cpp
@@ -2,24 +2,15 @@
 using namespace std;
 int main() {
 
-    int atz1,atz2,atz3;
+    int a,b,c,s1,s2,s3;
 
-    cout << "atz viens: ";
-    cin >> atz1;
-    cout << "atz divi: ";
-    cin >> atz2;
-    cout << "atz tris: ";
-    cin >> atz3;
+    cin >> a;
+    cin >> b;
+    cin >> c;
 
-    cout << atz1 << endl;
-    cout << atz2 << endl;
-    cout << atz3 << endl;
-
-    if(atz1 and atz2 > 50) {
-        cout << "Visas Sekmigas";
-    } else if(atz1 or atz2 or atz3 < 50) {
-        cout << ":(";
-    }
+    if(a<b) {
+        s1=a;
+    } else if()
 
     return 0;
     
